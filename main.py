8b1904d940d99a318151b1bf9f919ab63ab03b8e
Steve Wang <steve235lab@hotmail.com>
2023-07-07 05:30:59 +0800

    Add application entrance.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import uvicorn
 from fastapi import FastAPI
 
 from app.api import api_router
 
-app = FastAPI()
-app.include_router(api_router)
+if __name__ == "__main__":
+    app = FastAPI()
+    app.include_router(api_router)
+    uvicorn.run(app, port=8080, host="0.0.0.0")
